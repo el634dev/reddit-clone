f8d3c1b057d96ebfb276740e5cf4d5ae1e467830
el634dev <84924260+el634dev@users.noreply.github.com>
2024-11-25 18:05:33 -0500

    Updated reddit-db.js

--- data/reddit-db.js
+++ data/reddit-db.js
@@ -10,18 +10,13 @@ const connection_string = process.env.CONNECTION_STRING;
 const url = connection_string;
 
 // Connection Logic
-mongoose.connect(
-  url,
-  {
-    useNewUrlParser: true
-  },
-  function(err, db) {
-    assert.equal(null, err);
-    console.log('Connected successfully to database');
+mongoose.connect(url).catch((err) => {
+  //callback code
+  assert.equal(null, err);
+  console.log('Connected successfully to database');
+});
 
-    // db.close(); turn on for testing
-  }
-);
+
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
 mongoose.set('debug', true);
 
