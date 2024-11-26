9ee62d80348bfe32395d2e8fb258ede6dad7dfb1
el634dev <al7758066@gmail.com>
2024-11-25 19:06:51 -0500

    Users can see individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,15 +1,30 @@
 const Post = require('../models/post');
 
 module.exports = (app) => {
-    app.get("/", async (req, res) => {
+    app.get('/', async (req, res) => {
         try {
             const posts = await Post.find({}).lean();
             return res.render('posts-index', { posts })
         } catch (err) {
             console.log(err.message);
+            return res.status(500).send('Internal Server Error');
         };
     });
     
+    // Look Up The Post
+    app.get('/posts/:id', async (req, res) => {
+        try {
+            const post = await Post.findById(req.params.id).lean();
+
+            if (!post) {
+                return res.status(404).send('Post not found')
+            }
+            return res.render('posts-show', { post })
+        } catch (err) {
+            console.log(err.message);
+        };
+    })
+
     // CREATE
     app.post('/posts/new', (req, res) => {
         // Instantiate a new instance of POST model
